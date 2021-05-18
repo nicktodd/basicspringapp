1b324c0db6c506af862c995040f8f27e4e90505b
Nick Todd <nick.todd@conygre.com>
2021-05-18 12:59:10 +0100

    changed to curl to allow use from outside account

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -3,5 +3,6 @@ yum update -y,
 yum -y install java-1.8.0
 yum -y remove java-1.7.0-openjdk
 cd /home/ec2-user
-aws s3 cp s3://training.conygre.com/builds/sampleapp.jar /home/ec2-user/sampleapp.jar
+#aws s3 cp s3://training.conygre.com/builds/sampleapp.jar /home/ec2-user/sampleapp.jar
+curl http://training.conygre.com/builds/sampleapp.jar > sampleapp.jar
 nohup java -jar sampleapp.jar > ec2dep.log
